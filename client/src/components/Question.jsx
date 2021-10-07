c1d2e00fd10ee3127b8b71439bbfd21b018f438b
edpackard <edpackard81@gmail.com>
2021-10-07 20:04:14 +0100

    move quiz logic to question component

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -1,28 +1,88 @@
 import React from 'react'
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 
 export const Question = () => {
+  
+  const questions = [
+    {
+      questionText: "What is the capital of France?",
+      answerOptions: [
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "The iPhone was created by which company?",
+      answerOptions: [
+        { answerText: "Apple", isCorrect: true },
+        { answerText: "Intel", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Microsoft", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How many Harry Potter books are there?",
+      answerOptions: [
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false)
+  const [score, setScore] = useState(0);
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion); 
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
-    <>
-      <div className= "story-question"> 
-<Container>
-  <div class="card border-primary mb-3">
-  <div class="card-header">Question title</div>
-  <div class="card-body">
-    <h4 class="card-title">Question text lorem ipsum blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</h4>
-    <div class="d-grid gap-2">
-    <button class="btn btn-lg btn-primary" type="button">Question 1 button to select</button>
-    <button class="btn btn-lg btn-primary" type="button">Question 2 button to select</button>
-    <button class="btn btn-lg btn-primary" type="button">Question 3 button to select</button>
-    <button class="btn btn-lg btn-primary" type="button">Question 4 button to select</button>
+    <div>
+    {showScore ? (
+      <div className='score-section'>
+        You scored {score} out of {questions.length}
+      </div>
+    ) : (
+      <>
+        <Container>
+          <div class="card border-primary mb-3">
+            <div class="card-header">Question Number {currentQuestion + 1}</div>
+            <div class="card-body">
+              <h4 class="card-title">{questions[currentQuestion].questionText}</h4>
+              <div class="d-grid gap-2">
+                {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+		              <button class="btn btn-lg btn-primary" type="button" onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+	              ))}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </>
+    )}
     </div>
-
-  </div>
-</div>
-</Container>
-</div>
-
-    </>
   )
 }
 
