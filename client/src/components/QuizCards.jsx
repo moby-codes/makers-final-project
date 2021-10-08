bcfcfdce739910944402cd6750e9c6162ebaa089
ian k <kickthechair@gmail.com>
2021-10-08 17:11:33 +0100

    Quiz cards on home page block aligned

--- client/src/components/QuizCards.jsx
+++ client/src/components/QuizCards.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Container } from "react-bootstrap";
 
 const QuizCards = () => {
 
@@ -6,32 +7,42 @@ const QuizCards = () => {
 
   return (
     <>
-    <div className="qcard1"> 
-      <div class="card text-white bg-info mb-3">
-      <div class="card-header">Story Header</div>
-      <div class="card-body">
-      <h4 class="card-title">Quiz Name</h4>
-      <p class="card-text">details about what you will learn / story</p>
-      </div>
-      </div>
-     
-      </div>
-      <div class="card text-white bg-info mb-3">
-      <div class="card-header">Header</div>
-      <div class="card-body">
-      <h4 class="card-title">Info card title</h4>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
+
+      <div className="qcard">
+        <Container>
+          <div class="card text-white bg-info mb-3">
+            <div class="card-header">Story Header</div>
+              <div class="card-body">
+                <h4 class="card-title">Quiz Name</h4>
+                <p class="card-text">details about what you will learn / story</p>
+            </div>
+          </div>
+        </Container>
       </div>
 
-      <div class="card text-white bg-info mb-3">
-      <div class="card-header">Header</div>
-      <div class="card-body">
-      <h4 class="card-title">Info card title</h4>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
+      <div className="qcard">
+        <Container>
+          <div class="card text-white bg-info mb-3">
+            <div class="card-header">Story Header</div>
+              <div class="card-body">
+                <h4 class="card-title">Quiz Name</h4>
+                <p class="card-text">details about what you will learn / story</p>
+            </div>
+          </div>
+        </Container>
       </div>
 
+      <div className="qcard">
+        <Container>
+          <div class="card text-white bg-info mb-3">
+            <div class="card-header">Story Header</div>
+              <div class="card-body">
+                <h4 class="card-title">Quiz Name</h4>
+                <p class="card-text">details about what you will learn / story</p>
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
   )
 }
