c3bf8c78b2b26ed05e0832ad7ee44df420a6a4a2
moby-codes <mubashar.jaffri@gmail.com>
2021-10-08 15:40:30 +0100

    exported title to a seperate component, re added navbar, removed question and story card

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,22 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/footer";
 import Story from "./components/Story";
 import Question from "./components/Question";
+import Header from "./components/Header"
+//import {} from "react-router-bootstrap"
+import Title from "./components/Title"
 
 export default function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
   return (
     <>
+      <Container> <Header /> </Container>
       <div className="title">
         <Container>
-          <h1> Quiz </h1>
+          <Title />
         </Container>
       </div>
-      <Story currentQuestion={currentQuestion} />
-      <Question
-        currentQuestion={currentQuestion}
-        setCurrentQuestion={setCurrentQuestion}
-      />
+     
       <Footer />{" "}
     </>
   );
