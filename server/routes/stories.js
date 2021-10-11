4172380b0d2a837c029deaf3ede4338fb8bec05f
edpackard <edpackard81@gmail.com>
2021-10-11 16:46:38 +0100

    update comments

--- server/routes/stories.js
+++ server/routes/stories.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
-// recordRoutes is an instance of the express router.
+// storyRoutes is an instance of the express router.
 // We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
+// The router will be added as a middleware and will take control of requests starting with path /stories.
 const storyRoutes = express.Router();
 
 // This will help us connect to the database
@@ -11,7 +11,7 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-// This section will help you get a list of all the records.
+// This section will help you get a list of all the stories.
 storyRoutes.route("/stories").get(function (req, res) {
   let db_connect = dbo.getDb("storyDatabase");
   db_connect
@@ -23,7 +23,7 @@ storyRoutes.route("/stories").get(function (req, res) {
     });
 });
 
-// This section will help you get a single record by id
+// This section will help you get a single story by id
 storyRoutes.route("/stories/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
