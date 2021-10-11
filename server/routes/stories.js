c907db1cce9c4e9de3380592b86aa6d68aeb9382
ian k <kickthechair@gmail.com>
2021-10-11 16:24:20 +0100

    database connection working - able to read mongodb objects

--- server/routes/stories.js
+++ server/routes/stories.js
@@ -27,7 +27,7 @@ storyRoutes.route("/stories").get(function (req, res) {
 storyRoutes.route("/stories/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("records").findOne(myquery, function (err, result) {
+  db_connect.collection("stories").findOne(myquery, function (err, result) {
     if (err) throw err;
     res.json(result);
   });
