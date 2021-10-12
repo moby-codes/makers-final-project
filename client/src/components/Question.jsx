291c0c891df215676f1db63a28a9bb7b804f7c01
ian k <kickthechair@gmail.com>
2021-10-12 15:29:48 +0100

    kjsdnvjsadfnvjndvjn

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -8,7 +8,8 @@ export const Question = ({currentQuestion, setCurrentQuestion}) => {
   
   const [showScore, setShowScore] = useState(false)
   const [score, setScore] = useState(0);
-  console.log(questions[0])
+  // console.log(questions)
+
 
   const handleAnswerButtonClick = (isCorrect) => {
     if (isCorrect) {
--- client/src/dbquestions.js
+++ client/src/dbquestions.js
@@ -1,27 +1,27 @@
 const axios = require("axios");
 
-const axiosCall = async () => {
-  const result = await axios("http://localhost:5000/stories/");
-  //console.log(result.data);
-  return result.data;
-};
+// const axiosCall = () => {
+//   axios
+//   .get("http://localhost:5000/stories/")
+//   .then((response) => {
+//     console.log(response.data)
+//     return response.data
+//   })
+//   // .then((data) => {
+//   //   console.log(data)
+//   // })
+// }
 
-const questions = axiosCall();
+async function questions() {
+  try {
+    const response = await axios.get('http://localhost:5000/stories/');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
 
 export default questions;
-
-// axios
-//   .get("http://localhost:5000/stories/")
-//   .then(function (response) {
-//     // handle success
-//     console.log(response)
-//     const questions = response.data;
-//     //export default questions
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   });
