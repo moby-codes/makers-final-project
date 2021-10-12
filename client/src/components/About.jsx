bd9e1118b23008ea3d2099f04355409ec23707f6
ian k <kickthechair@gmail.com>
2021-10-12 18:36:07 +0100

    about page styled

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,26 +5,23 @@ export const About = () => {
     <>
     <div className="about_us_title">
       <Container>
-        <h1> Code4Fun </h1>
+        <h1 id="custom-font-2"><strong>About</strong></h1>
       </Container>
     </div>
     <Container>
-        <h3> Background: </h3>
+        <h3 id="about-page-title"><strong>Background:</strong></h3>
     </Container>
     <div className="about_us_paragraph">
       <Container>
         <p>
-          <h3> This app was created to develop a fun learning enviroment for kids. Introducing fundamental coding concepts, 
+          <h3 id="about-page-non-title"> This app was created to develop a fun learning enviroment for kids. Introducing fundamental coding concepts, 
           Children can go through various quizzes and answer the given questions. With multiple themes to choose from, coding has never been this fun and simple before. Code4Fun, putting the FUN back into fundamental concepts</h3>
         </p>
       </Container>
     </div>
       <Container>
-        <h2>Founders:</h2>
-          <p>Mubashar Ali</p>
-          <p>Ian King</p>
-          <p>Ed Packard</p>
-          <p>Graeme Stirling</p>
+        <h3 id="about-page-title"><strong>Founders:</strong></h3>
+          <p id="about-page-non-title">Mubashar Ali, Ian King, Ed Packard, Graeme Stirling</p>
       </Container>
     </>
   )
