a41a3fb9bf65bc90bebbf6f50636ed5cbae01109
Graeme Stirling <graemejstirling@gmail.com>
2021-10-13 12:10:51 +0100

    Added rendering conditional

--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -4,22 +4,24 @@ import { Container } from "react-bootstrap";
 const Story = ({currentQuestion, questions, showScore}) => {
 
   return (
-    
-    <>
-      <div className="story-card">  
-    <Container>
-      <div class="card bg-warning">
-        <div class="card-body">
-          <h4 class="custom-font-2 card-title">{questions[currentQuestion].storyTitle}</h4>
-          <h6 class="custom-font-2 card-subtitle mb-2">{questions[currentQuestion].storySubtitle}</h6>
-          {questions[currentQuestion].storyText.map((element) => (
-            <p class="custom-font-2 card-text">{element.text}</p>
-	        ))}
-      </div>
-    </div>
-    </Container>
-    </div>
-    </>
+    <b>
+      {showScore ? (<h1>Well Done !!!!</h1>) : (
+      <>
+        <div className="story-card">
+        <Container>
+          <div class="card bg-warning">
+            <div class="card-body">
+              <h4 class="custom-font-2 card-title">{questions[currentQuestion].storyTitle}</h4>
+              <h6 class="custom-font-2 card-subtitle mb-2">{questions[currentQuestion].storySubtitle}</h6>
+              {questions[currentQuestion].storyText.map((element) => (
+                <p class="custom-font-2 card-text">{element.text}</p>
+              ))}
+          </div>
+        </div>
+        </Container>
+        </div>
+     </>) }
+    </b>
   )
 }
 export default Story;
