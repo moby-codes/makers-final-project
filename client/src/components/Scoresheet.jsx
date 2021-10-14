3650e85d3470eeba9a9bc9692021dcfd992aaa1d
Graeme Stirling <graemejstirling@gmail.com>
2021-10-14 15:20:55 +0100

    Changed title fonts and spacing

--- client/src/components/Scoresheet.jsx
+++ client/src/components/Scoresheet.jsx
@@ -6,12 +6,12 @@ export const Scoresheet = ({results}) => {
 return (
   <>
   <div className="score-section">  
-  <h2> How did you do? </h2>
+  <h1 class="custom-font-4"> How did you do? </h1>
   {results.map((element, index) => 
     { return element ? (
-      <h3 class="custom-font-2">Question {index +1}: Correct - congratulations!</h3>
+      <h3 class="custom-font-4">Question {index +1}: Correct - congratulations!</h3>
     ) : (
-      <h3 class="custom-font-2">Question {index +1}: Bad luck - try again next time</h3>
+      <h3 class="custom-font-4">Question {index +1}: Bad luck - try again next time</h3>
     )}
   )}
     <Link to='/'>
