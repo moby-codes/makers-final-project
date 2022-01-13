acf195018f9d15ab3b922d6d68a7921d2092b5b7
Graeme Stirling <graemejstirling@gmail.com>
2022-01-13 16:37:51 +0000

    Wrapping routes inside switch tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
-// import logo from "./logo.svg";
 import { Container } from "react-bootstrap";
 import Footer from "./components/footer";
 import Header from "./components/Header";
-// import Title from "./components/Title"
 import QuizDashboard from "./components/QuizDashboard";
 import home from "./components/home";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SpaceQuiz from "./components/SpaceQuiz";
 import About from "./components/About";
 import Racecar from "./components/RaceCarQuiz";
 import Fantasy from "./components/Fantasy";
-// import Connection from "./db_axios"
+
 
 export default function App() {
   return (
@@ -20,19 +18,15 @@ export default function App() {
           <Header />
         </Container>
 
-        {/* <div className="title">
         <Container>
-          <Title />
-        </Container>
-      </div> */}
-
-        <Container>
-          <Route path="/" component={home} exact />
-          <Route path="/quiz-dashboard" component={QuizDashboard} exact />
-          <Route path="/spacequiz" component={SpaceQuiz} exact />
-          <Route path="/about" component={About} exact />
-          <Route path="/racecarsquiz" component={Racecar} exact />
-          <Route path="/fantasyquiz" component={Fantasy} exact />
+          <Switch>
+            <Route path="/" component={home} exact />
+            <Route path="/quiz-dashboard" component={QuizDashboard} exact />
+            <Route path="/spacequiz" component={SpaceQuiz} exact />
+            <Route path="/about" component={About} exact />
+            <Route path="/racecarsquiz" component={Racecar} exact />
+            <Route path="/fantasyquiz" component={Fantasy} exact />
+          </Switch >
         </Container>
         <footer>
           <Footer />
--- client/src/__test__/about.test.js
+++ client/src/__test__/about.test.js
@@ -1,11 +1,6 @@
 import { render } from "@testing-library/react";
 import About from "../components/About";
 
-// beforeEach(() => {
-//    const component = render(<About />);
-// });
-// first test is verifying about page for the content that we expect it to have. 
-
 test("renders title", () => {
   const component = render(<About />);
   const aboutEl = component.getByTestId("aboutTitle")
@@ -20,13 +15,11 @@ test("renders headings", () => {
   expect(backEl2.textContent).toBe("Founders:")
 });
 
-
 test("renders details about app", () => {
   const component = render(<About />);
   const backEl = component.getByTestId("aboutDetails")
   expect(backEl.textContent).toContain("This app was created")
 });
-
 
 test("renders the names of the founders", () => {
   const component = render(<About />);
@@ -34,24 +27,3 @@ test("renders the names of the founders", () => {
   expect(backEl.textContent).toContain("Ali")
 });
 
-
-
-// test("when user clicks on correct answer, score should increment", () => {
-//   const result = render(<App />);
-//   const answerButton = result.container.querySelector("#answer2");
-//   let scoreKeeper = result.container.querySelector("#score_keeper");
-//   act(() => {
-//     answerButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-//   });
-//   expect(scoreKeeper.getAttribute("data-score")).toEqual("1");
-// });
-
-// test("when user clicks on wrong answer, score should NOT increment", () => {
-//   const result = render(<App />);
-//   const answerButton = result.container.querySelector("#answer0");
-//   let scoreKeeper = result.container.querySelector("#score_keeper");
-//   act(() => {
-//     answerButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-//   });
-//   expect(scoreKeeper.getAttribute("data-score")).toEqual("0");
-// });
